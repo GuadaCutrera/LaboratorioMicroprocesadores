0c1d8daee28e2d38a7f4441dfc9aaaf1da3da17a
GuadaCutrera <128185694+GuadaCutrera@users.noreply.github.com>
2023-06-29 20:30:24 -0300

    Corregido

--- Entrega LCP1769 y MDE/Aplicacion.h
+++ Entrega LCP1769 y MDE/Aplicacion.h
@@ -1,35 +1,5 @@
-/*
- * Aplicacion.h
- *
- *  Created on: 5 dic. 2019
- *      Author: Ramiro Palomeque
- */
-
 #ifndef APLICACION_H_
 #define APLICACION_H_
-/*
-	#include "Puertos_InfoII.h"
-	#include "DR_GPIOs.h"
-	#include "FW_Oscilador.h"
-	#include "DR_Interrupciones.h"
-
-	#include "DR_Expansion2.h"
-	#include "DR_Expansion3.h"
-
-	#include "DR_ADC.h"
-	#include "DR_Teclado.h"
-	#include "DR_Relays.h"
-	#include "DR_RGB.h"
-	#include "DR_UARTs.h"
-	#include "DR_LCD.h"
-
-	#include "DR_RTC.h"
-	#include "DR_Systick.h"
-	#include "DR_Timers.h"
-	#include "PR_Temporizaciones.h"
-
-	#include "PR_Strings.h"
-*/
 
 //!< ////////////////// PRIMITIVAS  GENERALES //////////////////////////////
  	 void Inicializar_KIT( void );
@@ -47,12 +17,17 @@
  	 //void mde_key_status_init(void);
  	 void mde_key_status(void);
 
-
+    
  	 // MANEJO DE BITS
      int32_t ApagarBit(uint32_t *var, uint8_t nBit);
      int32_t EncenderBit(uint32_t *var, uint8_t nBit);
      uint8_t ConsultarBit(uint32_t *var, uint8_t nBit);
      int32_t InvertirBit(uint32_t *var, uint8_t nBit);
+
+    //otras funciones
+    uint8_t GetPIN();
+    void RGB_LEDXpresso(uint8_t nBit,uint8_t modo);
+
 
  	 typedef enum{
  		 ON,
@@ -84,7 +59,9 @@
 
 //!< //////////////////  DEFINES GENERALES	 //////////////////////////////
 
-
+    #define key_time 500 // 500 ms
+    #define OFF 0
+    #define ON 1
 
 
 
